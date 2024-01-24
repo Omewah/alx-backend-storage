a3dba364e5f7a8eb1f16f5e299edfc81f4969187
Omewah <joelomewah@ymail.com>
2024-01-24 19:11:58 +0100

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,20 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""Implementing an expiring web cache and tracker"""
 import redis
 import requests
-from functools import wraps
-from typing import Callable
+
+rdca = redis.Redis()
+count = 0
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """get the web cache data"""
+    rdca.set(f"cached:{url}", count)
+    pg_response = requests.get(url)
+    rdca.incr(f"count:{url}")
+    rdca.setex(f"cached:{url}", 10, rdca.get(f"cached:{url}"))
+    return pg_response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
